3f0ad02e461e751a0285d815e550bee91a6118e2
shauryaaXcode <shauryapratapsingh423@gmail.com>
2026-07-24 20:08:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (57.07%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-     vector<int> result(n+1);
-     if(n==0){
+        vector<int> result(n + 1);
+        if (n == 0) {
+            result[0] = 0;
+            return result;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            if (i % 2 != 0) {
+                result[i] = result[i / 2] + 1;
+            } else {
+                result[i] = result[i / 2];
+            }
+        }
         return result;
-        result[0] = 0;
-     }
-     for(int i = 1;i<n+1;i++){
-        if(i%2!=0){
-            result[i] = result[i/2]+1;
-        }
-        else{
-            result[i] = result[i/2];
-        }
-     }
-     return result;
-
     }
 };
