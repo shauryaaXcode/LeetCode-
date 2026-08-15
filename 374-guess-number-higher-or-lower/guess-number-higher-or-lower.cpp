1cf9de230ec155b856c24b1651fb0e015994dbba
shauryaaXcode <shauryapratapsingh423@gmail.com>
2026-08-15 15:48:23 +0530

    Time: 2 ms (56.31%) | Memory: 8 MB (6.75%) - LeetSync

---
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
@@ -0,0 +1,27 @@
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int s = 1;
+        int e = n;
+        while (s <= e) {
+            int guess_no = s + (e - s) / 2;
+            int val = guess(guess_no);
+            if (val == 0) {
+                return guess_no;
+            } else if (val == -1) {
+                e = guess_no - 1;
+            } else
+                s = guess_no + 1;
+        }
+        return -1;
+    }
+};
